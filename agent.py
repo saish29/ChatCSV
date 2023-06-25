608a1b099d10869b5d7048690866fab610629275
saish29 <saishmandavkar@live.com>
2023-06-25 19:07:31 -0400

    Set up agent and QA for LLM

--- agent.py
+++ agent.py
@@ -1,3 +1,95 @@
 # Setting Agent
 
 from langchain import OpenAI
+from langchain.agents import create_pandas_dataframe_agent
+import pandas as pd
+
+
+#Importing api key
+
+import environ
+
+env = environ.Env()
+environ.Env.read_env()
+
+API_KEY = env("apikey")
+
+
+
+# Creating agent
+
+def create_agent(filename :str):
+     """
+    Create an agent that can access and use a large language model (LLM).
+
+    Args:
+        filename: The path to the CSV file that contains the data.
+
+    Returns:
+        An agent that can access and use the LLM.
+    """
+     
+
+     # Creating an openai object
+     llmodel = OpenAI(openai_api_key=API_KEY)
+
+     # Reading the csv file uploaded by USER
+     df = pd.read_csv(filename)
+
+     return create_pandas_dataframe_agent(llmodel, df, verbose = False)
+
+
+# Query Agent
+
+def query_agent(agent, query):
+    """
+    Query an agent and return the response as a string.
+
+    Args:
+        agent: The agent to query.
+        query: The query to ask the agent.
+
+    Returns:
+        The response from the agent as a string.
+    """
+
+    prompt = (
+        """
+            For the following query, if it requires drawing a table, reply as follows:
+            {"table": {"columns": ["column1", "column2", ...], "data": [[value1, value2, ...], [value1, value2, ...], ...]}}
+
+            If the query requires creating a bar chart, reply as follows:
+            {"bar": {"columns": ["A", "B", "C", ...], "data": [25, 24, 10, ...]}}
+
+            If the query requires creating a line chart, reply as follows:
+            {"line": {"columns": ["A", "B", "C", ...], "data": [25, 24, 10, ...]}}
+
+            There can only be two types of chart, "bar" and "line".
+
+            If it is just asking a question that requires neither, reply as follows:
+            {"answer": "answer"}
+            Example:
+            {"answer": "The title with the highest rating is 'Gilead'"}
+
+            If you do not know the answer, reply as follows:
+            {"answer": "I do not know."}
+
+            Return all output as a string.
+
+            All strings in "columns" list and data list, should be in double quotes,
+
+            For example: {"columns": ["title", "ratings_count"], "data": [["Gilead", 361], ["Spider's Web", 5164]]}
+
+            Lets think step by step.
+
+            Below is the query.
+            Query: 
+            """
+        + query
+    )
+
+    # Run the prompt through the agent.
+    response = agent.run(prompt)
+
+    # Convert the response to a string.
+    return response.__str__()
